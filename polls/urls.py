c8afd8b0c3cfc43330cb29313fc5cd16b8777d21
Lei <gabriel_sl19798@hotmail.com>
2024-08-12 19:34:11 +1200

    create choice

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from polls.views import index, list_questions, question_detail
+from polls.views import index, list_questions, question_detail, create_question, create_choice
 
 urlpatterns = [
     path('', index, name='home'),
@@ -9,5 +9,9 @@ urlpatterns = [
     path('question_detail/<int:question_id>/',
          question_detail,
          name='question_detail'),
+    path('create_question/', create_question, name='create_question'),
+    path('create_choice/',
+         create_choice,
+         name='create_choice'),
 
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 # Create your views here.
@@ -20,3 +20,34 @@ def question_detail(request, question_id):
     return render(request,
                   'question_detail.html',
                   {'question': question})
+
+def create_question(request):
+    if request.method == 'POST':
+        question_text = request.POST.get('question_text')
+        question = Question(question_text=question_text)
+        question.save()
+        return render(request,
+                      'create_question.html',
+                      {'message': question.question_text
+                                  + " created"})
+    else:
+        return render(request,
+                      'create_question.html',
+                      {'message': ''})
+
+
+def create_choice(request):
+    if request.method == 'POST':
+        choice_text = request.POST.get('choice_text')
+        question_id = request.POST.get('question_id')
+        question = Question.objects.get(id=question_id)
+        print(question)
+        choice = Choice(question=question, choice_text=choice_text)
+        print(choice)
+        choice.save()
+        return render(request,
+                      'create_choice.html',
+                      {'message': choice.choice_text
+                                  + " created",
+                       'question_id': question_id})
+
